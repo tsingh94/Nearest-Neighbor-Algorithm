8652ef4fbb2a446ef73c4cf0493dfc03a132d8cd
tsingh94 <tsahn001@ucr.edu>
2015-11-03 15:36:13 -0800

    replaced file

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,6 +1,12 @@
+# Name: Teghveer Sahni
+# TA: Andrew Section: 21
+
 import sys
 import time
 
+# this function will read all of the points 
+# from the file and populate a list of tuples.
+# each tuple is one point with x and y coordinate
 def get_All_Points():
 	
 	points = []
@@ -17,17 +23,17 @@ def get_All_Points():
 
 	return points; 
 
-
+# this function runs the brute force algorithm for the closest pair.
+# it will return the smallest distance between any 2 points
 def brute_Force_Algo(all_Points):
 	
-	time_Start = time.time()
-
 	smallest_Dist = float("inf")
 
 	for i in range(0,len(all_Points)):
 		
 		for j in range((i+1),len(all_Points)):
 		
+			# calculating Euclidean distance between 2 points 
 			diff_X = all_Points[i][0] - all_Points[j][0]  
 			diff_Y = all_Points[i][1] - all_Points[j][1] 
 			distance = (diff_X**(2) + diff_Y**(2))**(.5)
@@ -35,63 +41,106 @@ def brute_Force_Algo(all_Points):
 			if distance < smallest_Dist:
 				smallest_Dist = distance
 
-	time_Stop = time.time()
-	print("Execution Time: ", time_Stop - time_Start)
+	return smallest_Dist
 
-	filename  = str(sys.argv[1])
-	file_Writer = open(filename[:len(filename)-4] + "_distance.txt", "w")
-	file_Writer.write(str(smallest_Dist))
-	file_Writer.close()
+	
+# this function calculates the distances which have one point on the
+# left side and one point on the right in the appropriate strip.
+# returns the smallest distance in the strip
+def find_Min_In_Strip(points_Sorted_By_Y, d, median):
+	
+	new_Point_List = []
 
-	#print("Smallest Distance is : ", smallest_Dist)
+	# removing all points that are not in the strip outlined by d
+	for i in range(0, len(points_Sorted_By_Y)):
+		
+		curr_Point = points_Sorted_By_Y[i][0]
+		 
+		if not((curr_Point > (median + d)) or (curr_Point < (median - d))):
+			new_Point_List.append(points_Sorted_By_Y[i])
 
+	
+	# calculating the smallest distance among the remaining points 
+	for i in range(0, len(new_Point_List)):
+		j = 1
+		#comparing an element with the next 7 elements next to it
+		while(((i + j) < len(new_Point_List)) and (j < 8)):
+			diff_X = new_Point_List[i][0] - new_Point_List[i+j][0]  
+			diff_Y = new_Point_List[i][1] - new_Point_List[i+j][1] 
+			distance = (diff_X**(2) + diff_Y**(2))**(.5)
 
+			if (distance < d):
+				distance = d
+			
+			j = j + 1
+
+	return d
+
+# this function is the main divide and conquer algorithm
+# this function calls the find_Min_In_Strip function
+# takes in all of the points that need to be compared and
+# returns the smallest distance
 def find_Smallest_Pair(points):
 	
-	print(points)
-	print("\n")
-	print (len(points))
-	print("\n")
-
 	if len(points) == 2 or len(points) == 3:
-		#diff_X = points[0][0] - points[1][0]
-		#diff_Y = points[0][1] - points[1][1]
-		#return (diff_X**(2) + diff_Y**(2))**(.5)
 		return brute_Force_Algo(points)
 
 	if len(points) % 2 == 0:
-		return max (find_Smallest_Pair(points[:int((len(points)/2))]), find_Smallest_Pair(points[int((len(points)/2)):]))
+		num1 = find_Smallest_Pair(points[:int((len(points)/2)+1)]) 
+		num2 = find_Smallest_Pair(points[int((len(points)/2)):])
+		d =  min(num1, num2)
 
 	else:
 		num1 = find_Smallest_Pair(points[:int(((len(points)-1)/2)+1)])
-		num2 = find_Smallest_Pair(points[int(((len(points)-1)/2)+1):])
+		num2 = find_Smallest_Pair(points[int(((len(points)-1)/2)):])
+		d =  min(num1, num2)
 
-		print("num1: ", num1)
-		print("\n")
-		print("num2: ", num2)
-		print("\n")
-
+	if len(points) % 2 == 0:
+		median = (len(points)/2) + 1
 
 
+	else:
+		median = points[int((len(points) + 1)/2)][0]
+	
+	points_Sorted_By_Y = sorted(points, key = lambda coordinate: coordinate[1])
 
+	return find_Min_In_Strip(points_Sorted_By_Y, d, median)
+
+
+# this function does some initializations for the divide and conquer algorithm to run
+# calls the find_Smallest_Pair function and returns the smallest distance
 def faster_Algo(all_Points):
 	
 	points_Sorted_By_X = sorted(all_Points, key = lambda coordinate: coordinate[0])
-	points_Sorted_By_Y = sorted(all_Points, key = lambda coordinate: coordinate[1])
+	
+	return find_Smallest_Pair(points_Sorted_By_X)
 
-	print (find_Smallest_Pair(points_Sorted_By_X))
-	#print (all_Points)
-	#print ("\n")
-	#print(points_Sorted_By_X)
-	#print ("\n")
-	#print (points_Sorted_By_Y)
-	#print ("\n")
-
-
-
+# getting all of the points from the file
 all_Points = get_All_Points()
-#faster_Algo(all_Points)
-brute_Force_Algo(all_Points)
 
+time_Start = time.time()
+
+smallest_Dist_Faster_Algo = faster_Algo(all_Points)
+
+time_Stop = time.time()
+
+print("Execution Time for Divide and Conquer Algorithm (secs): ", time_Stop - time_Start)
+
+time_Start = time.time()
+
+smallest_Dist_Brute_Force = brute_Force_Algo(all_Points)
+
+time_Stop = time.time()
+
+print("Execution Time for Brute Force Algorithm (secs): ", time_Stop - time_Start)
+
+#writing to the file
+filename  = str(sys.argv[1])
+file_Writer = open(filename[:len(filename)-4] + "_distance.txt", "w")
+file_Writer.write("Distance of closest pair calculated by divide and conquer algorithm: ")
+file_Writer.write(str(smallest_Dist_Faster_Algo))
+file_Writer.write("\nDistance of closest pair calculated by brute force algorithm: ")
+file_Writer.write(str(smallest_Dist_Brute_Force))
+file_Writer.close()
 
 
